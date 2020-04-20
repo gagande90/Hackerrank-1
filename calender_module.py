a901d52acab73bb5f61ecf4f6ee0b81e95511745
Gagan <49888313+gagande90@users.noreply.github.com>
2020-04-20 10:06:01 +1000

    Update calender_module.py
    Updated to the latest version Python3

--- calender_module.py
+++ calender_module.py
@@ -79,8 +79,8 @@ Day on 5th August 2015 is WEDNESDAY
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import calendar
-k = map(int,raw_input().split())
+k = list(map(int,input().split()))
 m = calendar.weekday(k[2],k[0],k[1])
 w = ['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY']
 
-print w[m]
+print(w[m])
